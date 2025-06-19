eba1278bbbdc1f49dece87dbbcb8b71fc98f342d
Suportes <suporte@vlcorrea.com.br>
2025-06-19 10:52:19 -0300

    acertando filtrar
    acertando filtrar

---
+++ areaRestrito/controller/cadastroUsuario/filtrar_usuarios.php
@@ -0,0 +1,16 @@
+<?php
+include("../../../config.php");
+
+$sql = "SELECT ID_USUARIO, USU_NOME, USU_STATUS, USU_EMAIL FROM CAD_USUARIO ORDER BY ID_USUARIO DESC";
+$result = $conn->query($sql);
+
+$usuarios = [];
+
+while ($row = $result->fetch_assoc()) {
+    $usuarios[] = $row;
+}
+
+echo json_encode($usuarios);
+
+$conn->close();
+?>
